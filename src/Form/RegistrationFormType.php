064f04e13fcac5616df47aefa4969b9c690b978a
Florian <michelflorian@yahoo.fr>
2021-05-02 23:10:09 +0200

    Add register user by admin

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,14 +3,21 @@
 namespace App\Form;
 
 use App\Entity\Users;
+use App\Entity\Roles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+// use Symfony\Bridge\Doctrine\Form\Type\ChoiceType;
+
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationFormType extends AbstractType
 {
@@ -18,30 +25,59 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
+            // ->add('plainPassword', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne sont pas identique',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter mot de passe']
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+            ->add('username')
+            ->add('name')
+            ->add('firstname')
+            // ->add('role', ChoiceType::class, [
+            //     'choices' => [
+            //         'Utilisateur' => 'user',
+            //         // 'Editeur' => 'editor',
+            //         // 'Administrateur' => 'ROLE_ADMIN'
+            //     ],
+            //     'expanded' => true,
+            //     'multiple' => false,
+            //     'label' => 'Rôle' 
+            // ])
+            // ->add('role', EntityType::class, [
+            //     'class' => Roles::class,
+            //     'choice_label' => 'name',
+                // 'empty_data' => ['2', 'user']
+                // 'choices' => ['2', 'user']
+            //     // 'data' => '2'
+                // 'choice_value' => '1',
+            // ])
         ;
     }
 
